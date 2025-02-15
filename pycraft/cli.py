1fcc32aafbedb8946eb92a152294843aac3d788e
N-Thander <nasiruddinthander2002@gmail.com>
2025-02-15 11:47:21 +0530

    cli.py updated for flask and django

--- pycraft/cli.py
+++ pycraft/cli.py
@@ -2,3 +2,23 @@
 import click
 
 from pycraft.setup_scripts import setup_flask
+from pycraft.setup_scripts import setup_django
+
+@click()
+def cli():
+    """PyCraft: Python Project Automation Tool"""
+    pass
+
+
+# Command to set up a Flask project
+@click.command()
+@click.argument("project_name")
+def setup_flask(project_name):
+    setup_flask(project_name)
+
+
+# Commad to setup a Django project
+@click.command()
+@click.argument("project_name")
+def setup_django(project_name):
+    setup_django(project_name)
